6182a89029fe99d768a22e91d39d4bfaba91b694
Deepak Kumar Sah <dkdear11@gmail.com>
2024-10-22 23:59:21 +0530

    Assignment Done

--- CODE2/JAVA1/Assignmet/circle.java
+++ CODE2/JAVA1/Assignmet/circle.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+import java.math.*;
+
 public class circle {
-    
+
+    // double radius;
+
+    // public void setValue( double radius) {
+    // this.radius = radius;
+    // }
+
+    Scanner sc = new Scanner(System.in);
+    double radius = sc.nextDouble();
+
+    public void getDetails() {
+        System.out.println("+--------+----------+-----------+");
+        System.out.println("Radius is :- " + radius);
+        System.out.println("Area is:- " + Math.PI * (radius * radius));
+        System.out.println("Parameter is :- " + 2 * Math.PI * radius);
+        System.out.println("+---------+--------+-----------+");
+    }
 }
--- CODE2/JAVA1/Assignmet/circleDrive.java
+++ CODE2/JAVA1/Assignmet/circleDrive.java
@@ -1,3 +1,17 @@
 public class circleDrive {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Enter Radius c1:- ");
+        circle c1 = new circle();
+        System.out.println("Enter Radius c2:- ");
+        circle c2 = new circle();
+        System.out.println("Enter Radius c3:- ");
+        circle c3 = new circle();
+        System.out.println("Enter Radius c4:- ");
+        circle c4 = new circle();
+        c1.getDetails();
+        c2.getDetails();
+        c3.getDetails();
+        c4.getDetails();
+    }
 }
