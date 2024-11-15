b2b33729dde130b6bef186d61df2960b636f89f5
Deepak Kumar Sah <dkdear11@gmail.com>
2024-11-15 14:40:47 +0530

    done

--- CODE2/JavaScript/taskSir.js
+++ CODE2/JavaScript/taskSir.js
@@ -1,4 +1,5 @@
 const productsWrapper = document.getElementById("productsWrapper");
+let cartItems = [];
 
 //! TO FETCH DATA FROM API
 async function fetchProducts() {
@@ -27,6 +28,25 @@ function DisplayProducts(products) {
     //! SETTING ATTRIBUTE
     card.setAttribute("class", "card");
     image.setAttribute("src", product.image);
+
+    add_to_cart_btn.addEventListener("click", () => {
+      console.log("item added to cart");
+      console.log(product);
+
+      // finding if product exists in cart or not
+      let existingProduct = cartItems.find((ele) => ele.id === product.id);
+
+      // if it exists, increase quantity +1
+      if (existingProduct) {
+        existingProduct.quantity += 1;
+      } else {
+        // push product in cart with a new key i.e, quantity
+        cartItems.push({ ...product, quantity: 1 });
+        add_to_cart_btn.textContent = "Add More";
+      }
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+      console.log(cartItems);
+    });
 
     card.append(
       image,
